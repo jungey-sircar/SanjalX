1a42f6b15531dde3141e3808156538cefe859310
jungey <voidmaneric@gmail.com>
2026-06-02 10:14:57 +0545

    fixed loading

--- backend/server.py
+++ backend/server.py
@@ -20,7 +20,7 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.environ.get('MONGO_URL', 'mongodb://localhost:27017')
 client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ.get('DB_NAME', 'connectx_db')]
 
